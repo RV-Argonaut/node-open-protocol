c261635f5007667234842cc0c65fe685e79b5495
John Sanders <jsanders@redviking.com>
2022-06-02 16:47:39 -0400

    add typedef for parser

--- src/openProtocolParser.d.ts
+++ src/openProtocolParser.d.ts
@@ -4,22 +4,27 @@ export interface OpenProtocolParserOpts extends TransformOptions {
     rawData?: boolean;
 };
 
-export type MID<PAYLOAD> = {
+export type MID<PAYLOAD, REV extends number = number> = {
     mid: number;
-    revision: number;
-    noAck: number;
+    revision: REV;
+    noAck: boolean;
+    /** @default 1 */
     stationID: number;
+    /** @default 1 */
     spindleID: number;
+    /** @default 0 */
     sequenceNumber: number;
+    /** @default 0 */
     messageParts: number;
+    /** @default 0 */
     messageNumber: number;
-    payload: Buffer;
+    payload: PAYLOAD;
     _raw?: Buffer;
 }
 
-export type OpenProtocolParserChunk = MID<Buffer>;
+export type EncodedMID = MID<Buffer>;
 
 export = class OpenProtocolParser extends Transform {
     constructor(opts?: OpenProtocolParserOpts): void;
-    on(event: 'data', listener: (chunk: OpenProtocolParserChunk) => void): void;
+    on(event: 'data', listener: (chunk: EncodedMID) => void): void;
 }
